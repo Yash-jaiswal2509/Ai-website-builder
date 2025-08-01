b0e00696e0dfd90d0c727b53ab4a8748ea73a4d4
Yash Jaiswal <Yashjaiswal2509@gmail.com>
2025-08-01 10:42:54 +0530

    (deploy): deployment

--- src/modules/home/ui/components/project-form.tsx
+++ src/modules/home/ui/components/project-form.tsx
@@ -7,7 +7,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import TextareaAutosize from 'react-textarea-autosize';
 import { ArrowUpIcon, Loader2Icon } from 'lucide-react';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { cn } from '@/lib/utils';
 import { useTRPC } from '@/trpc/client';
 import { Button } from '@/components/ui/button';
@@ -53,7 +53,7 @@ const ProjectForm = () => {
   );
 
   const [isFocused, setIsFocused] = useState<boolean>(false);
-  const showUsage = false;
+  // const showUsage = false;
   const isPending = createProject.isPending;
   const isButtonDisabled = isPending || !form.formState.isValid;
 
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,4 +1,3 @@
-import { z } from 'zod';
 import { createTRPCRouter } from '../init';
 import { messagesRouter } from '@/modules/messages/server/procedures';
 import { projectsRouter } from '@/modules/projects/server/procedures';
