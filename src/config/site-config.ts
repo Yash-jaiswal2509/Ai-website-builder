954311c5222315a50edf766f0fb9675302ae17f7
Yash Jaiswal <Yashjaiswal2509@gmail.com>
2025-08-01 11:52:37 +0530

    (site): added website info

---
+++ src/config/site-config.ts
@@ -0,0 +1,12 @@
+import { Metadata } from 'next';
+
+export const siteConfig: Metadata = {
+  title: 'Brixie - AI Website Builder',
+  description: 'Brixie turns natural language prompts into fully responsive websites using AI — no code, no hassle.',
+  icons: {
+    icon: './favicon.ico',
+  },
+  creator: 'Yash Jaiswal',
+  keywords: ['ai website builder', 'next.js', 'react', 'ai', 'website builder', 'brixie'],
+  category: 'technology',
+};
